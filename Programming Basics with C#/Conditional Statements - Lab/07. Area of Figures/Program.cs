cd64ea190388c8dfcae169ad389bb8fd8b42dde6
AntoniyaDimitrowa <antsimdim04@gmail.com>
2022-05-26 23:33:47 +0300

    Added another two projects from lesson 3

---
+++ Programming Basics with C#/Conditional Statements - Lab/07. Area of Figures/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace _07._Area_of_Figures
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string figures = Console.ReadLine();
+            if(figures == "square")
+            {
+                double a = double.Parse(Console.ReadLine());
+                Console.WriteLine($"{a * a:F3}");
+            }
+            if (figures == "rectangle")
+            {
+                double a = double.Parse(Console.ReadLine());
+                double b = double.Parse(Console.ReadLine());
+                Console.WriteLine($"{a * b:F3}");
+            }
+            if (figures == "circle")
+            {
+                double r = double.Parse(Console.ReadLine());
+                Console.WriteLine($"{Math.PI * r * r:F3}");
+            }
+            if (figures == "triangle")
+            {
+                double a = double.Parse(Console.ReadLine());
+                double h = double.Parse(Console.ReadLine());
+                Console.WriteLine($"{(a * h) / 2:F3}");
+            }
+        }
+    }
+}
