db0164775fcbcd4f786eff362656d0b0b72421aa
AntoniyaDimitrowa <antsimdim04@gmail.com>
2022-05-30 21:54:10 +0300

    Added projects from lesson 7

---
+++ Programming Basics with C#/For Loop - Lab/03. Numbers1ToNWithStep3/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _03._Numbers1ToNWithStep3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= num; i = i+3)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
---
+++ Programming Basics with C#/For Loop - Lab/04. EvenPowersOf2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _04._EvenPowersOf2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            for (int i = 0; i <= num; i = i + 2)
+            {
+                Console.WriteLine(Math.Pow(2,i));
+            }
+        }
+    }
+}
