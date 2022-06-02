ada3e95c1d82d3fbfe2cc6d9f58fd29be40c9a00
AntoniyaDimitrowa <antsimdim04@gmail.com>
2022-06-02 20:22:54 +0300

    Added projects from lesson 8

---
+++ Programming Basics with C#/For Loop - Exercise/02. HalfSumElement/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace _02._HalfSumElement
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int sum = 0;
+            int maxNum = int.MinValue;
+            for(int i = 1;i<= n;i++)
+            {
+                int num = int.Parse(Console.ReadLine());
+                sum = sum + num;
+                if (maxNum<num)
+                {
+                    maxNum = num;
+                }
+            }
+            int sumWithoutMaxNum = sum - maxNum;
+            if(maxNum== sumWithoutMaxNum)
+            {
+                Console.WriteLine($"Yes");
+                Console.WriteLine($"Sum = {sumWithoutMaxNum}");
+            }
+            else
+            {
+                Console.WriteLine($"No");
+                Console.WriteLine($"Diff = {Math.Abs(sumWithoutMaxNum - maxNum)}");
+            }
+        }
+    }
+}
---
+++ Programming Basics with C#/For Loop - Exercise/03. Histogram/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace _03._Histogram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int numbersFirstGroup = 0;
+            int numbersSecondGroup = 0;
+            int numbersThirdGroup = 0;
+            int numbersFourthGroup = 0;
+            int numbersFifthGroup = 0;
+            for (int i = 1; i <= n; i++)
+            {
+                int num = int.Parse(Console.ReadLine());
+                if (num<200)
+                {
+                    numbersFirstGroup++;
+                }
+                if (num >= 200 && num<400)
+                {
+                    numbersSecondGroup++;
+                }
+                if (num >= 400 && num < 600)
+                {
+                    numbersThirdGroup++;
+                }
+                if (num >= 600 && num < 800)
+                {
+                    numbersFourthGroup++;
+                }
+                if (num >= 800)
+                {
+                    numbersFifthGroup++;
+                }
+            }
+            //Console.WriteLine(numbersFirstGroup);
+            Console.WriteLine($"{(numbersFirstGroup*1.0) / n * 100:F2}%");
+            Console.WriteLine($"{(numbersSecondGroup * 1.0) / n * 100:F2}%");
+            Console.WriteLine($"{(numbersThirdGroup * 1.0) / n * 100:F2}%");
+            Console.WriteLine($"{(numbersFourthGroup * 1.0) / n * 100:F2}%");
+            Console.WriteLine($"{(numbersFifthGroup * 1.0) / n * 100:F2}%");
+        }
+    }
+}
