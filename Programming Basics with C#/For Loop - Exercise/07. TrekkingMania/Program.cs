e1305f45c4f69dfffdd7686fa9e02c7d6b89fe0f
AntoniyaDimitrowa <antsimdim04@gmail.com>
2022-06-04 10:43:59 +0300

    Added more projects from lesson 8

---
+++ Programming Basics with C#/For Loop - Exercise/07. TrekkingMania/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace _07._TrekkingMania
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfGroups = int.Parse(Console.ReadLine());
+            int numberOfAllPeople = 0;
+            int numberOfPeopleMusala = 0;
+            int numberOfPeopleMonblan = 0;
+            int numberOfPeopleKilimandjaro = 0;
+            int numberOfPeopleK2 = 0;
+            int numberOfPeopleEverest = 0;
+            for (int i = 1; i <= numberOfGroups; i++)
+            {
+                int numberOfGroupMembers = int.Parse(Console.ReadLine());
+                numberOfAllPeople = numberOfAllPeople + numberOfGroupMembers;
+                if(numberOfGroupMembers <= 5)
+                {
+                    numberOfPeopleMusala = numberOfPeopleMusala + numberOfGroupMembers;
+                }
+                if (numberOfGroupMembers>5 && numberOfGroupMembers <= 12)
+                {
+                    numberOfPeopleMonblan = numberOfPeopleMonblan + numberOfGroupMembers;
+                }
+                if (numberOfGroupMembers > 12 && numberOfGroupMembers <= 25)
+                {
+                    numberOfPeopleKilimandjaro = numberOfPeopleKilimandjaro + numberOfGroupMembers;
+                }
+                if (numberOfGroupMembers > 25 && numberOfGroupMembers <= 40)
+                {
+                    numberOfPeopleK2 = numberOfPeopleK2 + numberOfGroupMembers;
+                }
+                if (numberOfGroupMembers > 40)
+                {
+                    numberOfPeopleEverest = numberOfPeopleEverest + numberOfGroupMembers;
+                }
+            }
+            Console.WriteLine($"{(numberOfPeopleMusala * 1.0) / numberOfAllPeople * 100:F2}%");
+            Console.WriteLine($"{(numberOfPeopleMonblan * 1.0) / numberOfAllPeople * 100:F2}%");
+            Console.WriteLine($"{(numberOfPeopleKilimandjaro * 1.0) / numberOfAllPeople * 100:F2}%");
+            Console.WriteLine($"{(numberOfPeopleK2 * 1.0) / numberOfAllPeople * 100:F2}%");
+            Console.WriteLine($"{(numberOfPeopleEverest * 1.0) / numberOfAllPeople * 100:F2}%");
+        }
+    }
+}
---
+++ Programming Basics with C#/For Loop - Exercise/08. TennisRanklist/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace _08._TennisRanklist
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int numberOfTournaments = int.Parse(Console.ReadLine());
+            int pointsInTheBegining = int.Parse(Console.ReadLine());
+            int totalPoints = pointsInTheBegining;
+            int numberOfWins = 0;
+            for (int i = 1; i <= numberOfTournaments; i++)
+            {
+                string tournamentStage = Console.ReadLine();
+                if(tournamentStage =="W")
+                {
+                    totalPoints = totalPoints + 2000;
+                    numberOfWins++;
+                }
+                if (tournamentStage == "F")
+                {
+                    totalPoints = totalPoints + 1200;
+                }
+                if (tournamentStage == "SF")
+                {
+                    totalPoints = totalPoints + 720;
+                }
+            }
+            double averagePoints = (totalPoints - pointsInTheBegining) / (numberOfTournaments*1.0);
+            double average = Math.Floor(averagePoints);
+
+            Console.WriteLine($"Final points: {totalPoints}");
+            Console.WriteLine($"Average points: {average}");
+            Console.WriteLine($"{((numberOfWins*1.0)/numberOfTournaments)*100:F2}%");
+        }
+    }
+}
