ae947a5dd98db81b33758ec83bcca90d24806827
AntoniyaDimitrowa <antsimdim04@gmail.com>
2022-06-06 19:30:04 +0300

    Added more projects from lesson 9

---
+++ Programming Basics with C#/While Loop - Lab/P05. AccountBalance/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace P05._AccountBalance
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string command;
+            double sum = 0.0;
+            while ((command = Console.ReadLine())!= "NoMoreMoney")
+            {
+                double moneyToIncrease = double.Parse(command);
+                if (moneyToIncrease < 0)
+                {
+                    Console.WriteLine("Invalid operation!");
+                    break;
+                }
+                Console.WriteLine($"Increase: {moneyToIncrease:F2}");
+                sum += moneyToIncrease;
+            }
+            Console.WriteLine($"Total: {sum:F2}");
+        }
+    }
+}
