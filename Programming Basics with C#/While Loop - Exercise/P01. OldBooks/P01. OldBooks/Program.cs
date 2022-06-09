f9b06031f3cf493715c941ad292154792e00cb8b
AntoniyaDimitrowa <antsimdim04@gmail.com>
2022-06-09 13:24:40 +0300

    Added projects from lesson 10

---
+++ Programming Basics with C#/While Loop - Exercise/P01. OldBooks/P01. OldBooks/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace P01._OldBooks
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string favouriteBook = Console.ReadLine();
+            string book;
+            int cnt = 0;
+            while((book = Console.ReadLine())!= "No More Books")
+            {
+                if(book == favouriteBook)
+                {
+                    Console.WriteLine($"You checked {cnt} books and found it.");
+                    break;
+                }
+                cnt++;
+            }
+            if (book == "No More Books")
+            {
+                Console.WriteLine($"The book you search is not here!");
+                Console.WriteLine($"You checked {cnt} books.");
+            }
+        }
+    }
+}
---
+++ Programming Basics with C#/While Loop - Exercise/P02. ExamPreparation/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace P02._ExamPreparation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfBadGrades = int.Parse(Console.ReadLine());
+            int sum = 0;
+            int counterOfBadGrades = 0;
+            int counterOfAllGrades = 0;
+            string name = "";
+            string command;
+            while(true)
+            {
+                command = Console.ReadLine();
+                if (command == "Enough")
+                {
+                    break;
+                }
+                else
+                {
+                    name = command;
+                    int currGrade = int.Parse(Console.ReadLine());
+                    sum = sum + currGrade;
+                    counterOfAllGrades++;
+                    if (currGrade < 5)
+                    {
+                        counterOfBadGrades++;
+                        if (counterOfBadGrades >= numberOfBadGrades)
+                        {
+                            Console.WriteLine($"You need a break, {counterOfBadGrades} poor grades.");
+                            break;
+                        }
+                    }
+                }
+                
+            }
+            if (counterOfBadGrades < numberOfBadGrades)
+            {
+                Console.WriteLine($"Average score: {(sum * 1.0 / counterOfAllGrades):F2}");
+                Console.WriteLine($"Number of problems: {counterOfAllGrades}");
+                Console.WriteLine($"Last problem: {name}");
+            }
+        }
+    }
+}
