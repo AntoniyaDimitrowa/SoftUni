e146f6db4b3a4ae2c1c08709531a3c25a84c3256
AntoniyaDimitrowa <antsimdim04@gmail.com>
2022-06-11 23:50:39 +0300

    Added one more project from lesson 10

---
+++ Programming Basics with C#/While Loop - Exercise/P05. Coins/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace P05._Coins
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal change = decimal.Parse(Console.ReadLine());
+            int coinsCounter = 0;
+            while(change > 0.00m)
+            {
+                if(change>=2.00m)
+                {
+                    change = change - 2.00m;
+                }
+                else if (change >= 1.00m)
+                {
+                    change = change - 1;
+                }
+                else if(change>=0.50m)
+                {
+                    change = change - 0.50m; 
+                }
+                else if (change >= 0.20m)
+                {
+                    change = change - 0.20m;
+                }
+                else if (change >= 0.10m)
+                {
+                    change = change - 0.10m;
+                }
+                else if (change >= 0.05m)
+                {
+                    change = change - 0.05m;
+                }
+                else if (change >= 0.02m)
+                {
+                    change = change - 0.02m;
+                }
+                else if (change >= 0.01m)
+                {
+                    change = change - 0.1m;
+                }
+                coinsCounter++;
+            }
+            Console.WriteLine(coinsCounter);
+        }
+    }
+}
