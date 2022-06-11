8128c737e5656d0fa89582bf31c914b40906eee1
AntoniyaDimitrowa <antsimdim04@gmail.com>
2022-06-12 00:23:47 +0300

    Added the last two projects from lesson 10

---
+++ Programming Basics with C#/While Loop - Exercise/P06. Cake/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace P06._Cake
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int length = int.Parse(Console.ReadLine());
+            int width = int.Parse(Console.ReadLine());
+            int cakeS = length * width;
+            string command;
+            while((command = Console.ReadLine())!= "STOP")
+            {
+                int currPiecesNumber = int.Parse(command);
+                cakeS -= currPiecesNumber;
+                if(cakeS < 0)
+                {
+                    Console.WriteLine($"No more cake left! You need {Math.Abs(cakeS)} pieces more.");
+                    break;
+                }
+            }
+            if (cakeS > 0)
+            {
+                Console.WriteLine($"{cakeS} pieces are left.");
+            }
+                
+        }
+    }
+}
---
+++ Programming Basics with C#/While Loop - Exercise/P07. Moving/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace P07._Moving
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int length = int.Parse(Console.ReadLine());
+            int width = int.Parse(Console.ReadLine());
+            int height = int.Parse(Console.ReadLine());
+            int emptyArea = length * width * height;
+            string command;
+            while ((command = Console.ReadLine()) != "Done")
+            {
+                int currBoxesNumber = int.Parse(command);
+                emptyArea -= currBoxesNumber;
+                if(emptyArea < 0)
+                {
+                    Console.WriteLine($"No more free space! You need {Math.Abs(emptyArea)} Cubic meters more.");
+                    break;
+                }
+            }
+            if (emptyArea > 0)
+            {
+                Console.WriteLine($"{emptyArea} Cubic meters left.");
+            }
+        }
+    }
+}
