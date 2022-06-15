db99958195ed6b78fd8ea631b302d34d72f191f8
AntoniyaDimitrowa <antsimdim04@gmail.com>
2022-06-15 23:04:36 +0300

    Added projects from lesson 12

---
+++ Programming Basics with C#/Nested Loops - Exercise/P01. NumberPyramid/P01. NumberPyramid/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace P01._NumberPyramid
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int num = 1;
+
+            for (int row = 1; row <= n; row++)
+            {
+                for (int col = 1; col <= row; col++)
+                {
+                    if (num > n)
+                    {
+                        break;
+                    }
+                    Console.Write(num + " ");
+                    num++;
+                }
+                if (num > n)
+                {
+                    break;
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ Programming Basics with C#/Nested Loops - Exercise/P02. EqualSumsEvenOddPositions/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace P02._EqualSumsEvenOddPositions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNum = int.Parse(Console.ReadLine());
+            int secondNum = int.Parse(Console.ReadLine());
+
+            for (int i = firstNum; i <= secondNum; i++)
+            {
+                string currNum = i.ToString();
+                int sumOddPositions = 0;
+                int sumEvenPositions = 0;
+                for (int j = 0; j < currNum.Length; j++)
+                {
+                    if (j % 2 == 0)
+                    {
+                        sumEvenPositions += currNum[j];
+                    }
+                    else
+                    {
+                        sumOddPositions += currNum[j];
+                    }
+                }
+                if (sumOddPositions == sumEvenPositions)
+                {
+                    Console.Write(currNum + " ");
+                }
+            }
+        }
+    }
+}
