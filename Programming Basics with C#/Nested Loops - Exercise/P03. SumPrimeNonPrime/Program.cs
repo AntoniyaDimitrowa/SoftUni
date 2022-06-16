bbd60303de8c95c61a7dc8430dc43611ad0409ae
AntoniyaDimitrowa <antsimdim04@gmail.com>
2022-06-16 22:43:43 +0300

    Added more projects from lesson 12

---
+++ Programming Basics with C#/Nested Loops - Exercise/P03. SumPrimeNonPrime/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace P03._SumPrimeNonPrime
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int primeNumbersSum = 0;
+            int nonPrimeNumbersSum = 0;
+
+            string command;
+            while ((command = Console.ReadLine()) != "stop")
+            {
+                int currNum = int.Parse(command);
+
+                if (currNum < 0)
+                {
+                    Console.WriteLine("Number is negative.");
+                    continue;
+                }
+
+                bool isPrime = true;
+                for (int divider = 2; divider < currNum; divider++)
+                {
+                    if (currNum % divider == 0)
+                    {
+                        isPrime = false;
+                        nonPrimeNumbersSum += currNum;
+                        break;
+                    }
+                }
+                if (isPrime)
+                {
+                    primeNumbersSum += currNum;
+                }
+            }
+            Console.WriteLine($"Sum of all prime numbers is: {primeNumbersSum}");
+            Console.WriteLine($"Sum of all non prime numbers is: {nonPrimeNumbersSum}");
+        }
+    }
+}
---
+++ Programming Basics with C#/Nested Loops - Exercise/P04. TrainTheTrainers/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace P04._TrainTheTrainers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int juryMembers = int.Parse(Console.ReadLine());
+            string command;
+            double sumAllGrades = 0;
+            int presentationCounter = 0;
+            while ((command = Console.ReadLine()) != "Finish")
+            {
+                presentationCounter++;
+                string presentationName = command;
+                double sumCurrGrade = 0;
+                for (int i = 0; i < juryMembers; i++)
+                {
+                    sumCurrGrade += double.Parse(Console.ReadLine());
+                }
+                Console.WriteLine($"{presentationName} - {(sumCurrGrade / juryMembers):F2}.");
+                sumAllGrades += sumCurrGrade;
+            }
+            double finaleScore = sumAllGrades / (juryMembers * presentationCounter);
+            Console.WriteLine($"Student's final assessment is {finaleScore:F2}.");
+        }
+    }
+}
