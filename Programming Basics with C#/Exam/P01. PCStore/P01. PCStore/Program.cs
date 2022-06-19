1e37029d2497a8e1980ee48d1cf8c44fb659b03a
AntoniyaDimitrowa <antsimdim04@gmail.com>
2022-06-19 19:59:13 +0300

    Added some of my projects from the exam

---
+++ Programming Basics with C#/Exam/P01. PCStore/P01. PCStore/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace P01._PCStore
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double processorPriceUSD = double.Parse(Console.ReadLine());
+            double videoCardPriceUSD = double.Parse(Console.ReadLine());
+            double priceUSDOfRAMMemoryUSD = double.Parse(Console.ReadLine());
+            int numberOfRAM = int.Parse(Console.ReadLine());
+            double discount = double.Parse(Console.ReadLine());
+
+            double processorPriceBGN = processorPriceUSD * 1.57;
+            double videoCardPriceBGN = videoCardPriceUSD * 1.57;
+            double priceUSDOfRAMMemoryBGN = priceUSDOfRAMMemoryUSD * 1.57;
+
+            double totalPriceOfRAM = numberOfRAM * priceUSDOfRAMMemoryBGN;
+            double processorPriceAfterDiscount = processorPriceBGN - processorPriceBGN * discount;
+            double videoCardPriceAfterDiscount = videoCardPriceBGN - videoCardPriceBGN * discount;
+
+            double totalPrice = totalPriceOfRAM + videoCardPriceAfterDiscount + processorPriceAfterDiscount;
+            Console.WriteLine($"Money needed - {totalPrice:F2} leva.");
+        }
+    }
+}
---
+++ Programming Basics with C#/Exam/P02. FootballKit/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace P02._FootballKit
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double shirtPrice = double.Parse(Console.ReadLine());
+            double neededSumForBall = double.Parse(Console.ReadLine());
+
+            double shortsPrice = shirtPrice * 0.75;
+            double socksPrice = shortsPrice * 0.20;
+            double trainersPrice = (shirtPrice + shortsPrice) * 2;
+            double totalSum = shirtPrice + shortsPrice + socksPrice + trainersPrice;
+            double totalSumWithDiscount = totalSum - totalSum * 0.15;
+
+            if (totalSumWithDiscount >= neededSumForBall)
+            {
+                Console.WriteLine($"Yes, he will earn the world-cup replica ball!");
+                Console.WriteLine($"His sum is {totalSumWithDiscount:F2} lv.");
+            }
+            else
+            {
+                Console.WriteLine($"No, he will not earn the world-cup replica ball.");
+                Console.WriteLine($"He needs {(neededSumForBall - totalSumWithDiscount):F2} lv. more.");
+            }
+        }
+    }
+}
