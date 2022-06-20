9b1d85760ccc7eb313c19450d2d439b3e927d0b2
AntoniyaDimitrowa <antsimdim04@gmail.com>
2022-06-20 23:35:16 +0300

    Added more projects from my exam

---
+++ Programming Basics with C#/Exam/P03. ExcursionCalculator/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace P03._ExcursionCalculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfPeople = int.Parse(Console.ReadLine());
+            string season = Console.ReadLine();
+            double totalPrice = 0;
+
+            if (numberOfPeople <= 5)
+            {
+                if (season == "spring")
+                {
+                    totalPrice = numberOfPeople * 50.00;
+                }
+                else if (season == "summer")
+                {
+                    totalPrice = numberOfPeople * 48.50;
+                }
+                else if (season == "autumn")
+                {
+                    totalPrice = numberOfPeople * 60.00;
+                }
+                else if (season == "winter")
+                {
+                    totalPrice = numberOfPeople * 86.00;
+                }
+            }
+            else
+            {
+                if (season == "spring")
+                {
+                    totalPrice = numberOfPeople * 48.00;
+                }
+                else if (season == "summer")
+                {
+                    totalPrice = numberOfPeople * 45.00;
+                }
+                else if (season == "autumn")
+                {
+                    totalPrice = numberOfPeople * 49.50;
+                }
+                else if (season == "winter")
+                {
+                    totalPrice = numberOfPeople * 85.00;
+                }
+            }
+
+            if (season == "summer")
+            {
+                totalPrice = totalPrice - totalPrice * 0.15;
+            }
+            if (season == "winter")
+            {
+                totalPrice = totalPrice + totalPrice * 0.08;
+            }
+
+            Console.WriteLine($"{totalPrice:F2} leva.");
+        }
+    }
+}
---
+++ Programming Basics with C#/Exam/P04. ComputerFirm/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace P04._ComputerFirm
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int computerModelsNumber = int.Parse(Console.ReadLine());
+            int ratingsSum = 0;
+            double totalSales = 0;
+            for (int i = 1; i <= computerModelsNumber; i++)
+            {
+                int number = int.Parse(Console.ReadLine());
+                int rating = number % 10;
+                int possibleSales = number / 10;
+                double currSales = 0;
+
+                switch (rating)
+                {
+                    case 2:
+                        currSales = 0;
+                        break;
+                    case 3:
+                        currSales = possibleSales * 0.50;
+                        break;
+                    case 4:
+                        currSales = possibleSales * 0.70;
+                        break;
+                    case 5:
+                        currSales = possibleSales * 0.85;
+                        break;
+                    case 6:
+                        currSales = possibleSales;
+                        break;
+                }
+                totalSales += currSales;
+                ratingsSum += rating;
+            }
+            double averageRating = ratingsSum * 1.0 / computerModelsNumber;
+            Console.WriteLine($"{totalSales:F2}");
+            Console.WriteLine($"{averageRating:F2}");
+        }
+    }
+}
