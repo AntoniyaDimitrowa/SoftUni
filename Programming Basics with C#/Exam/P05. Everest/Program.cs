9b49e0c419435a3067d4533807bbc4bbdca119ce
AntoniyaDimitrowa <antsimdim04@gmail.com>
2022-06-21 22:09:27 +0300

    Added more projects from my exam

---
+++ Programming Basics with C#/Exam/P05. Everest/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace P05._Everest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string command = Console.ReadLine();
+            int daysCounter = 1;
+            int metersCounter = 5364;
+            bool isSucceeded = false;
+
+            while (command != "END")
+            {
+                string isSpendingTheNight = command.ToLower();
+                if (isSpendingTheNight == "yes")
+                {
+                    daysCounter++;
+                }
+                int climbedMeters = int.Parse(Console.ReadLine());
+                if (daysCounter > 5)
+                {
+                    isSucceeded = false;
+                    break;
+                }
+                metersCounter += climbedMeters;
+
+                if (metersCounter >= 8848)
+                {
+                    isSucceeded = true;
+                    break;
+                }
+
+                command = Console.ReadLine();
+            }
+
+            if (isSucceeded)
+            {
+                Console.WriteLine($"Goal reached for {daysCounter} days!");
+            }
+            else
+            {
+                Console.WriteLine("Failed!");
+                Console.WriteLine($"{metersCounter}");
+            }
+        }
+    }
+}
---
+++ Programming Basics with C#/Exam/P06. Substitute/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace P06._Substitute
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int k = int.Parse(Console.ReadLine());
+            int l = int.Parse(Console.ReadLine());
+            int m = int.Parse(Console.ReadLine());
+            int n = int.Parse(Console.ReadLine());
+            int validCounter = 0;
+
+
+            for (int i = k; i <= 8; i++)
+            {
+                for (int j = 9; j >= l; j--)
+                {
+                    int firstNum = i * 10 + j;
+
+                    for (int x = m; x <= 8; x++)
+                    {
+                        for (int y = 9; y >= n; y--)
+                        {
+                            int secondNum = x * 10 + y;
+                            if (i % 2 == 0 && j % 2 != 0 && x % 2 == 0 && y % 2 != 0)
+                            {
+
+                                if (firstNum == secondNum)
+                                {
+                                    Console.WriteLine($"Cannot change the same player."); ;
+                                    continue;
+                                }
+                                else
+                                {
+                                    validCounter++;
+                                    Console.WriteLine($"{i}{j} - {x}{y}");
+                                }
+                            }
+                            if (validCounter == 6)
+                            {
+                                break;
+                            }
+                        }
+                        if (validCounter == 6)
+                        {
+                            break;
+                        }
+                    }
+                    if (validCounter == 6)
+                    {
+                        break;
+                    }
+                }
+                if (validCounter == 6)
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
