c3a1b4ff01ebf753e2e2bac686468bb8799d606f
AntoniyaDimitrowa <antsimdim04@gmail.com>
2022-07-27 23:31:31 +0300

    Added file

---
+++ Programming Basics with Java/For-LoopMoreExercises/src/P02Hospital.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class P02Hospital {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int period = Integer.parseInt(scanner.nextLine());
+
+        for(int i = 1; i <= period; i++) {
+            if(i % 3 == 0) {
+
+            }
+        }
+    }
+}
