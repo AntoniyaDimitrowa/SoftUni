1d553149fa37c13f4c32b6e39fdc7455dfb7717c
AntoniyaDimitrowa <antsimdim04@gmail.com>
2022-11-02 23:13:32 +0200

    Added files

--- Programming Fundamentals with Java/ObjectsAndClasses_Exercise/src/P04_Students.java
+++ Programming Fundamentals with Java/ObjectsAndClasses_Exercise/src/P04_Students.java
@@ -1,6 +1,5 @@
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.lang.reflect.Array;
+import java.util.*;
 
 public class P04_Students {
     static class Student {
@@ -23,7 +22,7 @@ public class P04_Students {
         public double getGrade() {
             return this.grade;
         }
-        public void setGrade(int grade) {
+        public void setGrade(double grade) {
             this.grade = grade;
         }
 
@@ -37,14 +36,12 @@ public class P04_Students {
             return this.firstName + " " + this.lastName + ": " + this.grade;
         }
 
-        public void Swap(Student student) {
-
-        }
     }
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int numberOfStudents = Integer.parseInt(scanner.nextLine());
         List<Student> students = new ArrayList<>();
+        List<Double> grades = new ArrayList<>();
         for (int i = 0; i < numberOfStudents; i++) {
             String[] studentInfo = scanner.nextLine().split("\\s+");
             String firstName = studentInfo[0];
@@ -53,6 +50,18 @@ public class P04_Students {
 
             Student currStudent = new Student(firstName, lastName, grade);
             students.add(currStudent);
+
+            grades.add(grade);
+        }
+
+        Collections.sort(grades);
+        Collections.reverse(grades);
+        for (int i = 0; i < grades.size(); i++) {
+            for (int j = 0; j < students.size(); j++) {
+                if(grades.get(i) == students.get(j).grade) {
+                    System.out.printf("%s %s: %.2f%n", students.get(j).firstName, students.get(j).lastName, students.get(j).grade);
+                }
+            }
         }
     }
 }
