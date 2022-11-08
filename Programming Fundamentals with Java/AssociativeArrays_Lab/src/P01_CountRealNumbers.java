576e6734c9dc165ecad5ba0ecb0cb26a6531bb7d
AntoniyaDimitrowa <antsimdim04@gmail.com>
2022-11-08 23:47:56 +0200

    Added files

---
+++ Programming Fundamentals with Java/AssociativeArrays_Lab/src/P01_CountRealNumbers.java
@@ -0,0 +1,25 @@
+import java.text.DecimalFormat;
+import java.util.*;
+
+public class P01_CountRealNumbers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double[] numberList = Arrays.stream(scanner.nextLine().split(" "))
+                .mapToDouble(Double::parseDouble)
+                .toArray();
+
+        TreeMap<Double, Integer> counts = new TreeMap<>();
+
+        for(double num : numberList) {
+            if(!counts.containsKey(num)) {
+                counts.put(num, 0);
+            }
+            counts.put(num, counts.get(num) + 1);
+        }
+
+        for(Map.Entry<Double, Integer> entry : counts.entrySet()) {
+            DecimalFormat df = new DecimalFormat("#.#######");
+            System.out.printf("%s -> %d%n", df.format(entry.getKey()), entry.getValue());
+        }
+    }
+}
