ca5196098516d54eee76dafc27d387b7459835b2
AntoniyaDimitrowa <antsimdim04@gmail.com>
2022-11-19 23:21:37 +0200

    Added files

--- Programming Fundamentals with Java/RegularExpressions_Lab/src/P02_MatchPhoneNumber.java
+++ Programming Fundamentals with Java/RegularExpressions_Lab/src/P02_MatchPhoneNumber.java
@@ -1,18 +1,23 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class P01_MatchFullName {
+public class P02_MatchPhoneNumber {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String regex = "\\b[A-Z][a-z]+ [A-Z][a-z]+\\b";
+        String regex = "\\+359([ -])2\\1\\d{3}\\1\\d{4}\\b";
         String input = scanner.nextLine();
 
         Pattern pattern = Pattern.compile(regex);
         Matcher matcher = pattern.matcher(input);
 
+        List<String> phoneNumbers = new ArrayList<>();
         while (matcher.find()) {
-            System.out.print(matcher.group() + " ");
+            phoneNumbers.add(matcher.group());
         }
+
+        System.out.println(String.join(", ", phoneNumbers));
     }
 }
