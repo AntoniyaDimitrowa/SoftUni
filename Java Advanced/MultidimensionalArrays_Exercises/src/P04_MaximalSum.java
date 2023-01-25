36d1cb74e43a1ce4e2d6d0d86d312fbe1c04ab04
AntoniyaDimitrowa <antsimdim04@gmail.com>
2023-01-25 23:58:10 +0200

    Modified the last file

--- Java Advanced/MultidimensionalArrays_Exercises/src/P04_MaximalSum.java
+++ Java Advanced/MultidimensionalArrays_Exercises/src/P04_MaximalSum.java
@@ -8,6 +8,10 @@ public class P04_MaximalSum {
         int rows = Integer.parseInt(input[0]);
         int columns = Integer.parseInt(input[0]);
         int[][] matrix = getMatrix(rows, columns, scanner);
+        int[][] matrixWithMaxSum = findMatrixWithMaxSum(matrix);
+        int maxSum = getSumOf3x3Matrix(0, 0, matrixWithMaxSum);
+        System.out.println("Sum = " + maxSum);
+        matrixPrint(matrixWithMaxSum);
 
     }
 
@@ -23,11 +27,51 @@ public class P04_MaximalSum {
         return result;
     }
 
-    public static int[][] findMatrixWithBiggestSum(int[][] matrix) {
+    public static int[][] findMatrixWithMaxSum(int[][] matrix) {
+        int maxSum = Integer.MIN_VALUE;
+        int rowMaxSum = 0;
+        int columnMaxSum = 0;
         for (int row = 0; row < matrix.length - 2; row++) {
             for (int col = 0; col < matrix[0].length - 2; col++) {
-
+                int currSum = getSumOf3x3Matrix(row, col, matrix);
+                if(maxSum < currSum) {
+                    maxSum = currSum;
+                    rowMaxSum = row;
+                    columnMaxSum = col;
+                }
             }
+        }
+        int[][] result = new int[3][3];
+        for (int row = 0; row < 3; row++) {
+            for (int col = 0; col < 3; col++) {
+                result[row][col] = matrix[rowMaxSum][columnMaxSum];
+                columnMaxSum++;
+            }
+            rowMaxSum++;
+            columnMaxSum -= 2;
+        }
+        return result;
+    }
+
+    public static int getSumOf3x3Matrix(int r, int c, int[][] matrix) {
+        int sum = 0;
+        for (int row = r; row < r + 3; row++) {
+            for (int col = c; col < c + 3; col++) {
+                sum = sum + matrix[row][col];
+            }
+        }
+        return sum;
+    }
+
+    public static void matrixPrint (int[][] matrix) {
+        int rows = matrix.length;
+        int columns = matrix[0].length;
+
+        for (int row = 0; row < rows; row++) {
+            for (int col = 0; col < columns; col++) {
+                System.out.print(matrix[row][col] + " ");
+            }
+            System.out.println();
         }
     }
 }
